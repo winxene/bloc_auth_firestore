244b09080b86b1b310588ad71ae669372c9de652
winxene <winxeneric@gmail.com>
2023-12-24 15:22:30 +0700

    feat: added route for create screen

--- lib/router_manager.dart
+++ lib/router_manager.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_firestore_test/features/auth/presentations/pages/auth_page.dart';
+import 'package:flutter_auth_firestore_test/features/firestore/presentations/screens/firestore_create_screen.dart';
+import 'package:flutter_auth_firestore_test/features/firestore/presentations/screens/firestore_example_screen.dart';
 import 'package:flutter_auth_firestore_test/features/home/presentations/pages/home_page.dart';
 
 class RouterManager {
   final Map<String, WidgetBuilder> applicationRoutes = {
     AuthPage.routeName: (BuildContext context) => AuthPage(),
     HomePage.routeName: (BuildContext context) => HomePage(),
-    // Add more routes here
+    FirestoreExampleScreen.routeName: (BuildContext context) =>
+        FirestoreExampleScreen(),
+    FirestoreCreateScreen.routeName: (BuildContext context) =>
+        FirestoreCreateScreen(),
   };
 
   Map<String, WidgetBuilder> getApplicationRoutes() {
