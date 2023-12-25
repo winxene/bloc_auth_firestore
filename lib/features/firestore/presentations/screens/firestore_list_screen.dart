3fa896a5df0b2dc1355084559cd85e908e316a74
winxene <winxeneric@gmail.com>
2023-12-25 22:09:15 +0700

    refactor: changed from details to list

--- lib/features/firestore/presentations/screens/firestore_list_screen.dart
+++ lib/features/firestore/presentations/screens/firestore_list_screen.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 import '../../data/models/data_model.dart';
 import '../../../../core/firestore/firestore_manager.dart';
 import '../../domain/usecases/read_usecase.dart';
+import '../../domain/usecases/delete_usecase.dart';
 
-class FirestoreDetailScreen extends StatelessWidget {
-  static const String routeName = '/firestore-example/detail';
+class FirestoreListScreen extends StatelessWidget {
+  static const String routeName = '/firestore-example/list';
 
   final ReadUseCase _readUseCase = ReadUseCase(FirestoreManager());
+  final DeleteUseCase _deleteUseCase = DeleteUseCase(FirestoreManager());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Read Documents'),
+        title: const Text('List of'),
       ),
       body: StreamBuilder<List<DataModel>>(
         stream: _readUseCase.readDocuments('books'),
@@ -26,13 +28,21 @@ class FirestoreDetailScreen extends StatelessWidget {
             return const Text("Loading");
           }
 
-          return ListView(
-            children: snapshot.data!.map((DataModel model) {
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              final model = snapshot.data![index];
               return ListTile(
                 title: Text('Name: ${model.name}, Author: ${model.author}'),
                 subtitle: Text('Scores: ${model.score.toString()}'),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    _deleteUseCase.deleteDocument('books', model.id);
+                  },
+                ),
               );
-            }).toList(),
+            },
           );
         },
       ),
